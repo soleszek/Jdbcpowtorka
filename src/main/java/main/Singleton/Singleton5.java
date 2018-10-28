0fdcfb2005ba812a20545806b718d0bfd1f37823
Sylwester Oleszek <sylwester.oleszek@gmail.com>
2018-10-28 07:13:26 +0100

    Koniec nauki Singletona

---
+++ src/main/java/main/Singleton/Singleton5.java
@@ -0,0 +1,7 @@
+package main.Singleton;
+
+public class Singleton5 {
+    public static final Singleton5 INSTANCE = new Singleton5();
+
+    private Singleton5(){};
+}
---
+++ src/main/java/main/Singleton/Singleton6.java
@@ -0,0 +1,7 @@
+package main.Singleton;
+
+public class Singleton6 {
+    public static final Singleton6 INSTANCE = new Singleton6();
+
+    private Singleton6(){};
+}
