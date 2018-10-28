aaf68a3593a4c42ce81444ac22b99692982c3951
Sylwester Oleszek <sylwester.oleszek@gmail.com>
2018-10-28 08:01:43 +0100

    Baza - dalsza nauka

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -9,7 +9,8 @@ import java.sql.SQLException;
 public class Main {
     static Connection connection = JdbcUtils.getInstance().getConnection();
     public static void main(String[] args) throws SQLException {
-        insertRun(1l, "Bieg po złote runo", "Malawa", new java.util.Date(), new java.util.Date(), 100);
+        //insertRun(1l, "Bieg po złote runo", "Malawa", new java.util.Date(), new java.util.Date(), 100);
+        //insertMember(1l, "Sylwester", "Oleszek", 33, 1l);
     }
 
     private static void insertRun(Long id, String name, String place, java.util.Date startDate, java.util.Date startTime, int membersLimit) throws SQLException {
@@ -26,6 +27,19 @@ public class Main {
         statement.setDate(4, sqlStartDate);
         statement.setDate(5, sqlStartTime);
         statement.setInt(6, membersLimit);
+
+        statement.executeUpdate();
+    }
+
+    private static void insertMember(Long id, String name, String lastName, int startNumber, Long runId) throws SQLException {
+        PreparedStatement statement = connection
+                .prepareStatement("insert into members(id, name, last_name, start_number, run_id) values (?, ?, ?, ?, ?)");
+
+        statement.setLong(1, id);
+        statement.setString(2, name);
+        statement.setString(3, lastName);
+        statement.setInt(4, startNumber);
+        statement.setLong(5, runId);
 
         statement.executeUpdate();
     }
